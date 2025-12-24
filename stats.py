bc2ca9bb4361418aa4649a1fd1241b5999828b00
josmanov <jusuf.osmanovic01@gmail.com>
2025-12-24 18:54:01 +0200

    create letter counting function

--- stats.py
+++ stats.py
@@ -1,3 +1,37 @@
+letter_dict = {
+    'a': 0, 
+    'b': 0, 
+    'c': 0, 
+    'd': 0, 
+    'e': 0, 
+    'f': 0, 
+    'g': 0, 
+    'h': 0, 
+    'i': 0, 
+    'j': 0, 
+    'k': 0, 
+    'l': 0, 
+    'm': 0, 
+    'n': 0, 
+    'o': 0, 
+    'p': 0, 
+    'q': 0, 
+    'r': 0, 
+    's': 0, 
+    't': 0, 
+    'u': 0, 
+    'v': 0, 
+    'w': 0, 
+    'x': 0, 
+    'y': 0, 
+    'z': 0 
+}
+
+def count_chars(txt):
+    for i in txt:
+        if i in letter_dict:
+            letter_dict[i] + 1
+
 def count_words(txt):
     words_split = txt.split()
     return (len(words_split))
