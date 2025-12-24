cc8f82ddbcc8ac0a1ac24197399fb753a7f953d1
josmanov <jusuf.osmanovic01@gmail.com>
2025-12-24 18:29:35 +0200

    read text file and print text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    print(file_contents)
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+if __name__=="__main__":
+    main()
